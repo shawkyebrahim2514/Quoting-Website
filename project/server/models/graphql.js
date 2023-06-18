8d444754105938450e44e4f5c854c081429233f9
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-06-18 15:28:38 +0300

    Update fetch Nodejs problem

--- project/server/models/graphql.js
+++ project/server/models/graphql.js
@@ -1,11 +1,9 @@
+const fetch = require('isomorphic-fetch');
 // Get the graphql url from the .env file
 const baseUrl = process.env.NODE_ENV === 'production' ? process.env.HOST_DOMAIN : process.env.DEVELOPMENT_DOMAIN;
 const graphqlUrl = `${baseUrl}/graphql`;
 
 async function createRequest({ query, variables = {}, sessionId = null }) {
-    console.log("graphqlUrl: ", graphqlUrl);
-    console.log("ENV: ", process.env.NODE_ENV);
-    console.log("HOST_DOMAIN: ", process.env.HOST_DOMAIN);
     const response = await fetch(graphqlUrl, {
         method: "POST",
         headers: {
