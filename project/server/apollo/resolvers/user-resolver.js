b775aa6a04d0715758da5a867052fbd0c4442653
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-07-24 23:11:35 +0300

    Update user-resolver.js

--- project/server/apollo/resolvers/user-resolver.js
+++ project/server/apollo/resolvers/user-resolver.js
@@ -4,7 +4,7 @@ const userResolver = {
   User: {
     quotes: async ({ username }, { offset, loggedInUser }, context) => {
       try {
-        let quotes = await Quote.getAllQuotesByUsername(username, offset, loggedInUser);
+        let quotes = await Quote.getAllQuotesByUsername({ username, offset, loggedInUser });
         return quotes;
       } catch (error) {
         return [];
